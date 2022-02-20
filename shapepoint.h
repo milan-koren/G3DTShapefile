c651430d815da9b71e6ad081ffcaa9d1df3fdc80
milan-koren <milan.koren3@gmail.com>
2022-02-20 22:00:24 +0100

    Create shapepoint.h

---
+++ shapepoint.h
@@ -0,0 +1,31 @@
+#ifndef SHAPEPOINT_H
+#define SHAPEPOINT_H
+
+#include <QtCore>
+#include <g3dtshapefile_global.h>
+#include "shapebox.h"
+
+/*!
+ * *****************************************************************
+ *                            G3DTShapeFile
+ * *****************************************************************
+ * \file shapepoint.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTShapefile
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#pragma pack(2)
+
+struct G3DTSHAPEFILE_EXPORT ShapePoint
+{
+    double x; //!< x-coordinate
+    double y; //!< y-coordinate
+};
+
+#pragma pack()
+
+#endif // SHAPEPOINT_H
