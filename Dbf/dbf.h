8b6861c96260d5a972e7e9c279dbc8126a76d4e0
milan-koren <milan.koren3@gmail.com>
2022-02-24 12:28:58 +0100

    Create dbf.h

---
+++ Dbf/dbf.h
@@ -0,0 +1,22 @@
+#ifndef DBF_H
+#define DBF_H
+
+/*!
+ * *****************************************************************
+ *                            G3DTShapefile
+ * *****************************************************************
+ * \file dbf.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTShapefile
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "dbfcolumndescriptor.h"
+#include "dbffilecolumndescriptor.h"
+#include "dbffileheader.h"
+#include "dbffile.h"
+
+#endif // DBF_H
