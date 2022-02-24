91aab46163732b72f2b24a3307f773b539fa3192
milan-koren <milan.koren3@gmail.com>
2022-02-24 12:30:41 +0100

    Create shppolylinem.h

---
+++ Shp/shppolylinem.h
@@ -0,0 +1,43 @@
+#ifndef SHPPOLYLINEM_H
+#define SHPPOLYLINEM_H
+
+/*!
+ * *****************************************************************
+ *                            G3DTShapefile
+ * *****************************************************************
+ * \file shppolylinem.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTShapefile
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "shppolyline.h"
+
+
+class G3DTSHAPEFILE_EXPORT ShpPolyLineM : public ShpPolyLine
+{
+public:
+    double m0 = DBL_MAX;
+    double m1 = -DBL_MAX;
+    double *m = nullptr;
+
+public:   
+    ShpPolyLineM();
+    ShpPolyLineM(ShpPolyLineM &polyLineM);
+    virtual ~ShpPolyLineM() override;
+    ShpPolyLineM &operator=(ShpPolyLineM &polyLineM);
+
+    virtual void destroy() override;
+    void copyFrom(ShpPolyLineM &polyLineM);
+    virtual qint32 getTypeId() override;
+
+    virtual bool load(QFile &shpFile, qint64 shpOffset) override;
+
+protected:
+    void clean();
+};
+
+#endif // SHPPOLYLINEM_H
