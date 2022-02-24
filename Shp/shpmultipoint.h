afa6116a8fd59c27d79185a0b1e46556270ff3e2
milan-koren <milan.koren3@gmail.com>
2022-02-24 12:29:31 +0100

    Create shpmultipoint.h

---
+++ Shp/shpmultipoint.h
@@ -0,0 +1,43 @@
+#ifndef SHPMULTIPOINT_H
+#define SHPMULTIPOINT_H
+
+/*!
+ * *****************************************************************
+ *                            G3DTShapefile
+ * *****************************************************************
+ * \file shpmultipoint.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTShapefile
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "shpshape.h"
+
+
+class G3DTSHAPEFILE_EXPORT ShpMultiPoint : public ShpShape
+{
+public:
+    ShapeBox box;
+    qint32 nPoints = 0;
+    ShapePoint *points = nullptr;
+
+public:
+    ShpMultiPoint();
+    ShpMultiPoint(ShpMultiPoint &multiPoint);
+    virtual ~ShpMultiPoint() override;
+    ShpMultiPoint &operator=(ShpMultiPoint &multiPoint);
+
+    virtual void destroy() override;
+    void copyFrom(ShpMultiPoint &multiPoint);
+    virtual qint32 getTypeId() override;
+
+    virtual bool load(QFile &shpFile, qint64 shpOffset) override;
+
+protected:
+    void clean();
+};
+
+#endif // SHPMULTIPOINT_H
