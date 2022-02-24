7f45a22b01660a1e98f62d86d879fb343c83815b
milan-koren <milan.koren3@gmail.com>
2022-02-24 12:29:15 +0100

    Create shp.h

---
+++ Shp/shp.h
@@ -0,0 +1,32 @@
+#ifndef SHP_H
+#define SHP_H
+
+/*!
+ * *****************************************************************
+ *                            G3DTShapefile
+ * *****************************************************************
+ * \file shp.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTShapefile
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "shpshape.h"
+#include "shppoint.h"
+#include "shppointm.h"
+#include "shppointz.h"
+#include "shpmultipoint.h"
+#include "shpmultipointm.h"
+#include "shpmultipointz.h"
+#include "shppolyline.h"
+#include "shppolylinem.h"
+#include "shppolylinez.h"
+#include "shppolygon.h"
+#include "shppolygonm.h"
+#include "shppolygonz.h"
+#include "shpmultipatch.h"
+
+#endif // SHP_H
