97273daddbe8d3099ed81190d6be011d44af19e2
milan-koren <milan.koren3@gmail.com>
2022-02-24 12:30:00 +0100

    Create shppoint.h

---
+++ Shp/shppoint.h
@@ -0,0 +1,42 @@
+#ifndef SHPPOINT_H
+#define SHPPOINT_H
+
+/*!
+ * *****************************************************************
+ *                            G3DTShapefile
+ * *****************************************************************
+ * \file shppoint.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTShapefile
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "shpshape.h"
+
+
+class G3DTSHAPEFILE_EXPORT ShpPoint : public ShpShape
+{
+public:
+    double x = 0.0;
+    double y = 0.0;
+
+public:
+    ShpPoint();
+    ShpPoint(ShpPoint &point);
+    virtual ~ShpPoint() override;
+    ShpPoint &operator=(ShpPoint &point);
+
+    virtual void destroy() override;
+    void copyFrom(ShpPoint &point);
+    virtual qint32 getTypeId() override;
+
+    virtual bool load(QFile &shpFile, qint64 shpOffset) override;
+
+protected:
+    void clean();
+};
+
+#endif // SHPPOINT_H
